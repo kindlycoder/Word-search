a82e63c966fb62b324e80367a3a596c00eca9716
kindlycoder <30572579+kindlycoder@users.noreply.github.com>
2019-03-02 13:24:55 +0330

    add to the Matrix class: matrix param, getter, setter, and its constructor

--- src/Matrix.java
+++ src/Matrix.java
@@ -4,4 +4,37 @@
  */
 public class Matrix {
 	
+	/**
+	 * @author Noei
+	 */
+	private char matrix[][];
+	/**
+	 * Get matrix variable
+	 * @return Matrix
+	 * @author Noei
+	 */
+	public char[][] getMatrix() {
+		return matrix;
+	}
+
+	/**
+	 * Set matrix variable
+     * {@link Matrix#getMatrix()}
+     * @param matrix the given matrix
+     * @author Noei
+     */
+	public void setMatrix(char[][] matrix) {
+		this.matrix = matrix;
+	}
+
+	/**
+	 * The constructor that sets the matrix
+     * {@link Matrix#getMatrix()}
+     * @param matrix the given matrix
+     * @author Noei
+     */
+	public Matrix(char[][] matrix) {
+		super();
+		this.matrix = matrix;
+	}
 }
